70271338c320710bb0a41d862ba06a5118feedaa
PHITA\tranp <tranphita@gmail.com>
2018-11-06 22:47:57 +0700

    Get Data in Product -PhiTa

---
+++ Warehouse.UI/Models/Category/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Warehouse.Models.Category
+{
+    public class Class1
+    {
+    }
+}
---
+++ Warehouse.UI/Models/Product/ProductListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Warehouse.Entities;
+
+namespace Warehouse.Models
+{
+    public class ProductListModel
+    {
+        public int CurrentCategory { get; internal set; }
+        public int CurrentPage { get; internal set; }
+        public int PageCount { get; internal set; }
+        public int PageSize { get; internal set; }
+        public PagedList.IPagedList<Product> Products { get; set; }
+    }
+}
