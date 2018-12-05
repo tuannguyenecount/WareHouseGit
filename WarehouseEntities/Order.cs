bf5e39a70eb37b9ceb9c0a36cc6030ec14bf1a9d
Tuấn Nguyễn <nguyenaituan95@gmail.com>
2018-12-05 13:06:19 +0700

    update history order, ckeditor in blog

--- WarehouseEntities/Order.cs
+++ WarehouseEntities/Order.cs
@@ -22,37 +22,50 @@
         public string UserId { get; set; }
 
         [Required]
+        [Display(Name = "Tên khách")]
         public string Name { get; set; }
 
         [StringLength(256)]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Điện thoại")]
         public string Phone { get; set; }
 
+        [Display(Name = "Phường/Xã")]
         public int? WardId { get; set; }
 
+        [Display(Name = "Tỉnh/Thành")]
         public int? DistrictId { get; set; }
 
+        [Display(Name = "Quận/Huyện")]
         public int? ProvinceId { get; set; }
 
+        [Display(Name = "Địa chỉ")]
         public string Address { get; set; }
 
+        [Display(Name = "Tổng số lượng")]
         public int? TotalQuantity { get; set; }
 
         [Column(TypeName = "money")]
+        [Display(Name = "Tổng thành tiền")]
         public decimal? TotalMoney { get; set; }
 
+        [Display(Name = "Đã thanh toán?")]
         /// <summary>
         /// Đã thanh toán?
         /// </summary>
+        /// 
         public bool Paid { get; set; }
 
+        [Display(Name = "Trạng thái đơn hàng")]
         /// <summary>
         /// Trạng thái đơn hàng (0: chờ xác nhận, 1: đang giao hàng, 2: đã giao, 3: đổi/trả hàng, 4: bị huỷ)
         /// </summary>
         public byte Status { get; set; }
 
+        [Display(Name = "Ngày đặt hàng")]
         public DateTime DateOrder { get; set; }
 
         public virtual AspNetUser AspNetUser { get; set; }
